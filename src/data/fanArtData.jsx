2c25e3d134f497b130c8baf05862ed4af78138e5
Wiston Pastor <wiston.pastor@unmsm.edu.pe>
2025-11-26 09:52:05 -0500

    fix key fron masonry

---
+++ src/data/fanArtData.jsx
@@ -0,0 +1,56 @@
+export const fanArtImages =[
+    {
+        id:1,
+        publicId:"pintor-nocturno_lu4lhm",
+        alt:"Pintor nocturno",
+        artist:"witon",
+    },
+    {
+        id:2,
+        publicId:"images_ttx2lv",
+        alt:"tres chicos",
+        artist:"witon",
+    },
+    {
+        id:3,
+        publicId:"f598b92fbe001205d52016d7f0bdbed3_mujcoj",
+        alt:"personaje principal de pintor nocturno",
+        artist:"witon",
+    },
+    {
+        id:4,
+        publicId:"ea21759f8c59d7b67c6b0e74fbe5380f_fk0mjh",
+        alt:"chico da un beso",
+        artist:"witon",
+    },
+    {
+        id:5,
+        publicId:"bj-alex-destacada-1024x651-1700527292_sxxs3h",
+        alt:"dos amigos",
+        artist:"witon",
+    }
+    ,{
+        id:6,
+        publicId:"bjalex_bif1us",
+        alt:"bisnes man",
+        artist:"witon",
+    }
+    ,{
+        id:7,
+        publicId:"betweenloveandfriend_orwpyo",
+        alt:"chico en primera plana",
+        artist:"witon",
+    },
+    {
+        id:8,
+        publicId:"1628833639573_qnjvpc",
+        alt:"chico pirando fijamente",
+        artist:"witon"
+    },
+    {
+        id:9,
+        publicId:"6514567281ea7_vbw8el",
+        alt:"amigos abranzandose",
+        artist:"no es witon",
+    }
+]
--- src/features/Masonry.jsx
+++ src/features/Masonry.jsx
@@ -1,7 +1,31 @@
-function Masonry(){
-    return(
-        <div>
-            <h1>Masonry</h1>
+import { fanArtImages } from "@/data/fanArtData"
+
+const CLOUD_NAME = "duml5xr5i";
+
+function Masonry() {
+
+    const getOptimizedUrl = (publicId) => {
+
+        const baseUrl = `https://res.cloudinary.com/${CLOUD_NAME}/image/upload`;
+
+        const transformations = "f_auto,q_auto,w_800,c_limit";
+
+        return `${baseUrl}/${transformations}/${publicId}`;
+    };
+
+    return (
+        <div className="p-4 bg-pink-50 max-h-screen">
+            <h2 className="text-2xl  mb-8 text-center">
+                Mi coleccion BL
+            </h2>
+            
+            <div className="columns-1 sm:columns-2 lg:columns-3 xl:columns-4 gap-1 space-y-4 mx-auto max-w-5xl">
+                {fanArtImages.map((art)=>(
+                    <div key={art.id} >
+                        <img src={getOptimizedUrl(art.publicId)} alt={art.alt} />
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
